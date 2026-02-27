109792812b30fb3d9e8925b5b07811f1f85039eb
Hedgestock <pamarthur.aito@gmail.com>
2026-02-27 11:35:08 +0700

    Updated engine with more rendered 2D physics stuff

--- Scenes/2d_test.gd
+++ Scenes/2d_test.gd
@@ -8,13 +8,3 @@ func _unhandled_input(event):
 	
 		new_object.position = event.position
 		add_child(new_object)
-
-func _on_check_button_toggled(toggled_on):
-	var collision_shapes_list: Array[Node] = get_tree().get_root().find_children(
-		"", 
-		"CollisionShape2D", 
-		true, 
-		false
-	)
-	for shape in collision_shapes_list:
-		shape.rendering = toggled_on
--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -1,4 +1,8 @@
 extends CanvasLayer
 
-func _on_d_button_pressed():
+func _on_2d_button_pressed():
 	get_tree().change_scene_to_file("uid://cye2jy0air6ev")
+
+
+func _on_3d_button_pressed():
+	get_tree().change_scene_to_file("uid://bohkal6j228yl")
